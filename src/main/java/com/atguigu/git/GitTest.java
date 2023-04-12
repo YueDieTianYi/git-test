b43157dfb22f1f6d2fc606be43721d7f6228520b
bwm@atguigu.com <2528831847@qq.com>
2023-04-12 18:23:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.atguigu.git;
+
+public class GitTest {
+    //psvm
+    public static void main(String[] args) {
+        // "hello git.sout"
+        System.out.println("hello git!");
+    }
+}
