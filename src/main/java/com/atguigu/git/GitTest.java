c978f53ccfa86443912d6510e011bd2e8e845487
bwm@atguigu.com <2528831847@qq.com>
2023-04-12 18:52:29 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         // "hello git.sout"
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
